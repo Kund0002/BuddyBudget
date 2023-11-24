ce4ef51435df1fd604329318c4f3ed9867d0daa1
DominicNr2 <dongu21@student.sdu.dk>
2023-11-24 12:28:32 +0100

    added navigation

---
+++ app/src/main/java/com/example/buddybudget/Screen.kt
@@ -0,0 +1,15 @@
+package com.example.buddybudget
+
+sealed class Screen(val route: String) {
+    object MainScreen : Screen("main_screen")
+    object LoginScreen : Screen("login_screen")
+
+    fun withArgs(vararg args: String): String {
+        return buildString {
+            append(route)
+            args.forEach { arg ->
+                append("/$arg")
+            }
+        }
+    }
+}

613d8585f691cb66c7f8cbec18749f9fdbb14dd0
sabildm <90703231+sabildm@users.noreply.github.com>
2024-01-06 10:00:27 +0100

    Added UI features

--- app/src/main/java/com/example/buddybudget/NewGroup.kt
+++ app/src/main/java/com/example/buddybudget/NewGroup.kt
@@ -3,12 +3,14 @@ package com.example.buddybudget
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -21,7 +23,33 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewGroupScreen(navController: NavController) {
+    Row (horizontalArrangement = Arrangement.Center){
+        Column{
+            Text(text = "Group Name")
+            TextField(
+                value = "",
+                onValueChange = {it},
+                label = {Text("Enter name of group")}
+                )
+            Text(text = "Group Description")
+            TextField(
+                value = "",
+                onValueChange = {it},
+                label = {Text("Enter description for group")}
+            )
+            Text(text = "Search for Participants")
+            TextField(
+                value = "",
+                onValueChange = {it},
+                label = {Text("Add Person to Group")}
+            )
+            Button(onClick = {}) {
+                Text(text = "CREATE GROUP")
+            }
+        }
+    }
 
 }

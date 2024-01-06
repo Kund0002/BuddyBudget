c0e584a366e7ac64f09dcecaa6a25d4c3b6ad25e
DominicNr2 <dongu21@student.sdu.dk>
2024-01-06 23:55:29 +0100

    applied styling for signup and login screens to match the figma prototype

--- app/src/main/java/com/example/buddybudget/LoginScreen.kt
+++ app/src/main/java/com/example/buddybudget/LoginScreen.kt
@@ -4,14 +4,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,14 +23,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(navController: NavController) {
-    var username by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
     Column(
@@ -35,45 +41,58 @@ fun LoginScreen(navController: NavController) {
             .fillMaxWidth()
             .padding(horizontal = 50.dp)
     ) {
+        Spacer(modifier = Modifier.height(100.dp))
         Image(
             painter = painterResource(id = R.drawable.loginlogo),
             contentDescription = "Logo",
             modifier = Modifier.fillMaxWidth()
         )
-        Text(text = "Email")
-        TextField(
-            value = username,
-            onValueChange = { username = it },
-            modifier = Modifier.fillMaxWidth()
+
+        OutlinedTextField(
+            value = email, onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
         )
-        Text(text = "Password")
-        TextField(
-            value = password,
-            onValueChange = { password = it },
-            modifier = Modifier.fillMaxWidth()
+        OutlinedTextField(
+            value = password, onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
         )
+
         Spacer(modifier = Modifier.height(8.dp))
         Button(
             onClick = { navController.navigate(Screen.HomeScreen.route) },
-            modifier = Modifier.align(Alignment.End)
+            colors = ButtonDefaults.buttonColors(Color.Cyan),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .fillMaxWidth()
+                .weight(1f, fill = false)
+                .padding(5.dp)
+                .padding(horizontal = 50.dp)
         ) {
-            Text(text = "To Login Screen")
+            Text(text = "Login", fontSize = 25.sp)
         }
-        SignUpButton(navController)
 
+        Text(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .clickable { navController.navigate(Screen.SignUpScreen.route) },
+            text = "Don't have an account? Sign up"
+        )
         Button(
             onClick = { navController.navigate(Screen.UserInformationScreen.route) },
-            modifier = Modifier.align(Alignment.End)
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .fillMaxWidth()
+                .weight(1f, fill = false)
+                .padding(5.dp)
+                .padding(horizontal = 50.dp)
         ) {
-            Text(text = "To User Info")
+            Text(text = "To User Info", fontSize = 15.sp)
         }
     }
-}
-
-@Composable
-fun SignUpButton(navController: NavController) {
-    Text(
-        modifier = Modifier.clickable { navController.navigate(Screen.SignUpScreen.route) },
-        text = "Don't have an account? Sign up"
-    )
 }

1e9d413d1aaaf2d72149cb5a0bd52057f53be3e3
DominicNr2 <dongu21@student.sdu.dk>
2024-01-07 22:23:42 +0100

    restyled new group/jeans page

--- app/src/main/java/com/example/buddybudget/NewGroupScreen.kt
+++ app/src/main/java/com/example/buddybudget/NewGroupScreen.kt
@@ -1,15 +1,20 @@
 package com.example.buddybudget
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -19,48 +24,101 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewGroupScreen(navController: NavController) {
-    val name by remember { mutableStateOf("") }
-    val description by remember { mutableStateOf("") }
+    val Roman = FontFamily(Font(R.font.crimson_bold, FontWeight.Normal))
+    var groupName by remember { mutableStateOf("") }
+    var groupDesc by remember { mutableStateOf("") }
+    var participants by remember { mutableStateOf("") }
 
 
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 50.dp)
+    ) {
+        NavBar(navController, "username" )
+        Image(
+            painter = painterResource(id = R.drawable.back),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .clickable { navController.navigate(Screen.HomeScreen.route) }
+                .fillMaxWidth()
+        )
 
-        Column(
+        Text(
+            fontFamily = Roman,
+            fontSize = 25.sp,
+            color = Color.Cyan,
+            modifier = Modifier.padding(15.dp),
+            text = "Group Name"
+        )
+
+        OutlinedTextField(
+            value = groupName, onValueChange = { groupName = it },
+            label = { Text("Enter name of group") },
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(5.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            Text(text = "Group Name")
-            TextField(
-                value = "",
-                onValueChange = {},
-                label = {Text("Enter name of group")}
-                )
-            Text(text = "Group Description")
-            Spacer(modifier = Modifier.height(10.dp))
-            TextField(
-                value = "",
-                onValueChange = {},
-                label = {Text("Enter description for group")}
-            )
-            Text(text = "Search for Participants")
-            Spacer(modifier = Modifier.height(10.dp))
+                .padding(10.dp)
+        )
 
-            TextField(
-                value = "",
-                onValueChange = {},
-                label = {Text("Add Person to Group")}
-            )
-            Spacer(modifier = Modifier.height(10.dp))
+        Text(
+            fontFamily = Roman,
+            fontSize = 25.sp,
+            color = Color.Cyan,
+            modifier = Modifier.padding(15.dp),
+            text = "Group Description"
+        )
 
-            Button(onClick = {}) {
-                Text(text = "CREATE GROUP")
-            }
+        OutlinedTextField(
+            value = groupDesc, onValueChange = { groupDesc = it },
+            label = { Text("Enter description for group") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        )
+
+        Text(
+            fontFamily = Roman,
+            fontSize = 25.sp,
+            color = Color.Cyan,
+            modifier = Modifier.padding(15.dp),
+            text = "Search for Participants"
+        )
+
+
+        OutlinedTextField(
+            value = participants, onValueChange = { participants = it },
+            label = { Text("Add Person to Group") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        )
+
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(Color.Cyan),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .fillMaxWidth()
+                .weight(1f, fill = false)
+                .padding(5.dp)
+                .padding(horizontal = 30.dp)
+        ) {
+            Text(text = "CREATE GROUP", fontSize = 20.sp)
         }
     }
+}

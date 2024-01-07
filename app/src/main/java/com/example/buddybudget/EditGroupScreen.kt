ed1b3db8e6c72bde239ee1407e6176266fb0edea
sabildm <90703231+sabildm@users.noreply.github.com>
2024-01-07 16:27:35 +0100

    nothing really

--- app/src/main/java/com/example/buddybudget/EditGroupScreen.kt
+++ app/src/main/java/com/example/buddybudget/EditGroupScreen.kt
@@ -1,42 +1,54 @@
 package com.example.buddybudget
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavController
 
+@SuppressLint("UnrememberedMutableState")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditGroupScreen(navController: NavController, name: String?) {
-    Row (horizontalArrangement = Arrangement.Center){
-        Column{
-            Text(text = "Group Name")
-            TextField(
-                value = "",
-                onValueChange = {it},
-                label = { Text("Enter name of group") }
-            )
-            Text(text = "Group Description")
-            TextField(
-                value = "",
-                onValueChange = {it},
-                label = { Text("Enter description for group") }
-            )
-            Text(text = "Search for Participants")
-            TextField(
-                value = "",
-                onValueChange = {it},
-                label = { Text("Add Person to Group") }
-            )
-            Button(onClick = {}) {
-                Text(text = "CREATE GROUP")
-            }
-            Text(text = "Group Members")
+    Column{
+        Text(text = "Group Name")
+        TextField(
+            value = "",
+            onValueChange = {it},
+            label = { Text("Enter name of group") }
+        )
+        Text(text = "Group Description")
+        TextField(
+            value = "",
+            onValueChange = {it},
+            label = { Text("Enter description for group") }
+        )
+        Text(text = "Search for Participants")
+        TextField(
+            value = "",
+            onValueChange = {it},
+            label = { Text("Add Person to Group") }
+        )
+        Button(onClick = {}) {
+            Text(text = "CREATE GROUP")
         }
+        Text(text = "Group Members")
     }
 }
+
+
+
+
